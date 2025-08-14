0746a2db04e642ff11dd56cef1ba6e6e0d9805f2
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-08-14 16:42:21 -0500

    Arreglos de test

--- src/app/m-product/product-label-feedback-page/product-label-feedback-page.component.spec.ts
+++ src/app/m-product/product-label-feedback-page/product-label-feedback-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LayoutModule } from 'src/app/layout/layout.module';
+import { ProductCommonModule } from 'src/app/m-product/product-common/product-common.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NgbDropdownModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService, ToastrModule, TOAST_CONFIG } from 'ngx-toastr';
 
 import { ProductLabelFeedbackPageComponent } from './product-label-feedback-page.component';
@@ -12,7 +16,15 @@ describe('ProductLabelFeedbackPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, HttpClientTestingModule ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        LayoutModule,
+        ProductCommonModule,
+        SharedModule,
+        NgbDropdownModule,
+        NgbPaginationModule
+      ],
       declarations: [ ProductLabelFeedbackPageComponent ],
       providers: [
         { 
--- src/app/user/register-activation/register-activation.component.spec.ts
+++ src/app/user/register-activation/register-activation.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RegisterActivationComponent } from './register-activation.component';
 
@@ -8,6 +9,7 @@ describe('RegisterActivationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ RegisterActivationComponent ]
     })
     .compileComponents();
