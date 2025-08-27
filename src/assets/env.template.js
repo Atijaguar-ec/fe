788784d6d35eee814f2aba1bf4f324d2882e7bb7
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-08-27 10:46:23 -0500

    Configurar env

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -6,14 +6,14 @@
     // Use JS fallbacks so empty substitutions still yield sane defaults in production
     window['env']['environmentName'] = ('${ENVIRONMENT_NAME}') || 'production';
     window['env']['appBaseUrl'] = ('${APP_BASE_URL}') || '';
-    window['env']['qrCodeBasePath'] = ('${QR_CODE_BASE_PATH}') || '/api/stock-order';
-    window['env']['relativeFileUploadUrl'] = ('${RELATIVE_FILE_UPLOAD_URL}') || '/api/document';
-    window['env']['relativeFileUploadUrlManualType'] = ('${RELATIVE_FILE_UPLOAD_URL_MANUAL_TYPE}') || '/api/document?type=';
+    window['env']['qrCodeBasePath'] = ('${QR_CODE_BASE_PATH}') || 'q-cd';
+    window['env']['relativeFileUploadUrl'] = ('${RELATIVE_FILE_UPLOAD_URL}') || '/api/common/document';
+    window['env']['relativeFileUploadUrlManualType'] = ('${RELATIVE_FILE_UPLOAD_URL_MANUAL_TYPE}') || '/api/common/document?type=';
     window['env']['relativeImageUploadUrl'] = ('${RELATIVE_IMAGE_UPLOAD_URL}') || '/api/common/image';
     window['env']['relativeImageUploadUrlAllSizes'] = ('${RELATIVE_IMAGE_UPLOAD_URL_ALL_SIZES}') || '/api/common/image'
-    window['env']['googleMapsApiKey'] = ('${GOOGLE_MAPS_API_KEY}') || '';
+    window['env']['googleMapsApiKey'] = ('${GOOGLE_MAPS_API_KEY}') || 'AIzaSyAP1JuiYWi0A_Zf8BK0YIfl4nCKoxHnPHU';
     window['env']['tokenForPublicLogRoute'] = ('${TOKEN_FOR_PUBLIC_LOG_ROUTE}') || '';
-    window['env']['mapboxAccessToken'] = ('${MAPBOX_ACCESS_TOKEN}') || '';
+    window['env']['mapboxAccessToken'] = ('${MAPBOX_ACCESS_TOKEN}') || 'pk.eyJ1IjoiYWx2YXJvZ2VvdmFuaSIsImEiOiJjbWN5bDFkbG0wcGt4Mm5xNngydnZ0cTUxIn0.e15Wl5VmuU4S2QIiO5242A';
 
     // Environment variables for Beyco integration
     window['env']['beycoAuthURL'] = ('${BEYCO_AUTH_URL}') || ''
