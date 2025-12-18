3e89d169f65c0f4a28144fa2c49966e6c1ba248e
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-12-18 17:38:24 -0500

    Ajustar

--- generate-api.js
+++ generate-api.js
@@ -2,8 +2,12 @@ var fs = require('fs');
 var converter = require('api-spec-converter');
 var child_process = require('child_process');
 var fetch = require("node-fetch");
+require('dotenv').config();
 
-var swagger_docs_host = 'http://localhost:8080/v3/api-docs'
+// Allow overriding from environment for CI/CD or different environments
+var swagger_docs_host = process.env.SWAGGER_DOCS_HOST
+
+//192.168.100.250:8080/v3/api-docs
 
 function afterBuild(code) {
     if (code === 0) {
